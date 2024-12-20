d11816a03709048c61b850e44baf7d2c1706d2ab
Affan Rayyan <affan.harjanto@gmail.com>
2024-12-20 21:06:09 +0700

    Add files via upload

---
+++ anim.js
@@ -0,0 +1,55 @@
+// Sincronizar las letras con la canción
+var audio = document.querySelector("audio");
+var lyrics = document.querySelector("#lyrics");
+
+// Array de objetos que contiene cada línea y su tiempo de aparición en segundos
+var lyricsData = [
+  { text: "dan kau hadir, merubah segalanya", time: 0 },
+  { text: "menjadi lebih indah", time: 5 },
+  { text: "kau bawa cintaku setinggi angkasa", time: 10 },
+  { text: "membuatku merasa sempurna", time: 15 },
+  { text: "dan membuatku utuh", time: 20 },
+  { text: "tuk menjalani hidup", time: 25 }, 
+  { text: "berdua dengan mu selama-lamanya", time: 30 },
+  { text: "kaulah yang terbaik untukku", time: 35 },
+
+ 
+];
+
+// Animar las letras
+function updateLyrics() {
+  var time = Math.floor(audio.currentTime);
+  var currentLine = lyricsData.find(
+    (line) => time >= line.time && time < line.time + 6
+  );
+
+  if (currentLine) {
+    // Calcula la opacidad basada en el tiempo en la línea actual
+    var fadeInDuration = 0.1; // Duración del efecto de aparición en segundos
+    var opacity = Math.min(1, (time - currentLine.time) / fadeInDuration);
+
+    // Aplica el efecto de aparición
+    lyrics.style.opacity = opacity;
+    lyrics.innerHTML = currentLine.text;
+  } else {
+    // Restablece la opacidad y el contenido si no hay una línea actual
+    lyrics.style.opacity = 0;
+    lyrics.innerHTML = "";
+  }
+}
+
+setInterval(updateLyrics, 1000);
+
+//funcion titulo
+// Función para ocultar el título después de 216 segundos
+function ocultarTitulo() {
+  var titulo = document.querySelector(".titulo");
+  titulo.style.animation =
+    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+  setTimeout(function () {
+    titulo.style.display = "none";
+  }, 3000); // Espera 3 segundos antes de ocultar completamente
+}
+
+// Llama a la función después de 216 segundos (216,000 milisegundos)
+setTimeout(ocultarTitulo, 216000);
